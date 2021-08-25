fabe7bc5c1611ed0f748aa7876d97e09ce9d3a19
Elnatan999 <84679871+Elnatan999@users.noreply.github.com>
2021-08-25 23:30:56 +0300

    commit simple shell

--- f_handle_enter.c
+++ f_handle_enter.c
@@ -6,6 +6,7 @@
  *
  * Return: 1 if no command was entered(just spaces and ENTER), 0 otherwise
 */
+
 int handle_enter(char **commands)
 {
 	if (commands[0] == NULL || _strcmp(commands[0], "\n") == 0)
